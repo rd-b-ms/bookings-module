f020726149015859310944e137c8d5c9158c25f0
Michelle <michelledhuang@ucla.edu>
2019-05-14 22:47:49 -0700

    Set up frontend and backend skeleton for module

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('./client/dist'));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  module: {
+    entry: './path/to/my/entry/file.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'my-first-webpack.bundle.js'
+    },
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
