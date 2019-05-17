bdf13590a47e0d5914bd0f7660dc86f5758ddaeb
Michelle <michelledhuang@ucla.edu>
2019-05-17 11:49:35 -0700

    Add styling to Calendar, create month year header and buttons for going to previous and next month

--- client/src/calendar.jsx
+++ client/src/calendar.jsx
@@ -2,13 +2,44 @@ import React from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
 
-const WeekdayHeader = styled.th`
-  font-size: smaller;
+const CalendarTable = styled.table`
+  border-collapse: collapse;
+`;
+
+const HeaderButton = styled.td`
   font-family: Roboto, sans-serif;
   text-align: center;
   color: rgb(117, 117, 117);
-  padding-left: 5px;
-  padding-right: 5px;
+  border: 1px solid rgb(228, 231, 231);
+`;
+
+const MonthYearHeader = styled.td`
+  font-weight: 700;
+  font-family: Roboto, sans-serif;
+  text-align: center;
+  color: rgb(72, 72, 72);
+`;
+
+const WeekdayHeader = styled.th`
+  font-weight: 400;
+  font-size: 12px;
+  font-family: Roboto, sans-serif;
+  text-align: center;
+  color: rgb(117, 117, 117);
+  width: 40px;
+  padding-bottom: 3px;
+  padding-top: 15px;
+`;
+
+const DayGrid = styled.td`
+  font-size: 12px;
+  font-weight: 700;
+  font-family: Roboto, sans-serif;
+  border: 1px solid rgb(228, 231, 231);
+  text-align: center;
+  color: rgb(72, 72, 72);
+  width: 40px;
+  height: 40px;
 `;
 
 class Calendar extends React.Component {
@@ -52,7 +83,15 @@ class Calendar extends React.Component {
     return firstDay;
   }
 
-  createHeader() {
+  createMonthYearHeader() {
+    return (
+      <MonthYearHeader colSpan="5">
+        <span>{`${this.getMonth()} ${this.getYear()}`}</span>
+      </MonthYearHeader>
+    );
+  }
+
+  createWeekDayHeader() {
     return this.weekdaysShort.map(day => (
       <WeekdayHeader key={day} className="week-day">
         {day.substring(0, 2)}
@@ -70,7 +109,7 @@ class Calendar extends React.Component {
     }
 
     for (let j = 1; j <= this.getDaysInMonth(); j += 1) {
-      bodyArr.push(<td className="day" key={`day-${j}`}><span>{j}</span></td>);
+      bodyArr.push(<DayGrid className="day" key={`day-${j}`}><span>{j}</span></DayGrid>);
     }
 
     let row = [];
@@ -93,17 +132,23 @@ class Calendar extends React.Component {
 
   render() {
     return (
-      <div className="calendar-container">
-        <table className="calendar">
-          <thead>
-            <tr className="calendar-header"><th>May</th></tr>
-          </thead>
-          <tbody>
-            <tr>{this.createHeader()}</tr>
-            {this.createBody()}
-          </tbody>
-        </table>
-      </div>
+      <CalendarTable className="calendar">
+        <thead>
+          <tr className="calendar-header">
+            <HeaderButton>
+              <span>←</span>
+            </HeaderButton>
+            {this.createMonthYearHeader()}
+            <td>
+              <span>→</span>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>{this.createWeekDayHeader()}</tr>
+          {this.createBody()}
+        </tbody>
+      </CalendarTable>
     );
   }
 }
