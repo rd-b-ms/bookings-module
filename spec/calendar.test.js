1fe5d0ac620f66d184020232ca7af8a2460d072e
Michelle Huang <michelledhuang@g.ucla.edu>
2019-05-27 21:43:07 -0700

    Fix date comparison bug and create testing for calendar component

--- spec/calendar.test.js
+++ spec/calendar.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import jest from 'jest-mock';
 import { shallow, mount } from 'enzyme';
 import moment from 'moment';
 import Calendar from '../client/src/calendar';
@@ -25,6 +26,22 @@ describe('Calendar', () => {
 
   it("Checks if Calendar's today is today", () => {
     const wrapper = initializeCalendar();
-    
+    const today = moment().format('MM/DD/Y');
+    expect(wrapper.state().today.format('MM/DD/Y')).toEqual(today);
+  });
+
+  it('Check today on Calendar can be selected', () => {
+    const wrapper = initializeCalendar();
+    const today = moment().format('MMDY');
+    const beforeToday = moment().subtract(1, 'days').format('MMDY');
+    wrapper.find(`td#date${today}`).simulate('click');
+    expect(wrapper.update().state().fromDate).toBeTruthy();
+  });
+
+  it('Check day before today on Calendar can not be selected', () => {
+    const wrapper = initializeCalendar();
+    const beforeToday = moment().subtract(1, 'days').format('MMDY');
+    wrapper.find(`td#date${beforeToday}`).simulate('click');
+    expect(wrapper.update().state().fromDate).toBeNull();
   });
 });
