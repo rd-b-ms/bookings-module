6ef9d74bf3c1b4d3acf20ca0bd8cd75fe2ef2086
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-06-07 14:49:05 -0700

    Not use faker, and not commit any cruft.

--- db/seedCSVs.js
+++ db/seedCSVs.js
@@ -2,21 +2,18 @@
 /* eslint-disable func-names */
 /* eslint-disable no-console */
 const fs = require('fs');
-const csvWriter = require('csv-write-stream');
 
-const writer1 = csvWriter();
-const writer2 = csvWriter();
 const seededData = require('./generateData');
 
 const insertListingRecords = function () {
   const listingRecords = seededData.generateListings();
 
   const dataGen = () => {
-    writer1.pipe(fs.createWriteStream('listings_data.csv'));
+    const listingsFile = fs.createWriteStream('listings_data.csv');
     for (let i = 0; i < 10000000; i += 1) {
-      writer1.write(listingRecords[i]);
+      listingsFile.write(listingRecords[i]);
     }
-    writer1.end();
+    listingsFile.end();
     console.log('LISTINGS done!');
   };
 
@@ -27,11 +24,11 @@ const insertBookingRecords = function () {
   const bookingRecords = seededData.generateBookings();
 
   const dataGen = () => {
-    writer2.pipe(fs.createWriteStream('bookings_data5.csv'));
+    const bookingsFile = fs.createWriteStream('bookings_data.csv');
     for (let i = 0; i < 100000000; i += 1) {
-      writer2.write(bookingRecords[i]);
+      bookingsFile.write(bookingRecords[i]);
     }
-    writer2.end();
+    bookingsFile.end();
     console.log('BOOKINGS done!');
   };
 
